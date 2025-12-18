d9a1eade8a8ae026195ce552c5f9ebc0637a61ec
Boby Siddana <bobys@gmail.com>
2025-12-18 11:48:51 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z062034436HMDE861EBQF"
+}
+
+variable "zone_name" {
+  default = "dawsj.space"
+}
+
+variable "component" {
+
+}
+
+variable "rule_priority" {
+    
+}
